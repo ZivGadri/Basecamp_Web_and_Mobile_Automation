6d1ccd595b34e74903048952fc97bc147828276c
Ziv Gadri <ziv.automation@gmail.com>
2020-06-05 11:19:30 +0300

    Creating Methods initWeb and initMobile under ManagePages class

--- src/main/java/Utilities/ManagePages.java
+++ src/main/java/Utilities/ManagePages.java
@@ -1,4 +1,29 @@
-package Utilities;
+package Utilities;              //A class that will define an object for each page/section class of the app and initialize the
+                                // objects we define in each and every one. Inherits from Base class
 
-public class ManagePages {
+import org.openqa.selenium.support.PageFactory;
+
+public class ManagePages extends Base{
+
+    public static void initWeb() {
+        basecampIntroPage = PageFactory.initElements(driver, PageObjects.Web.IntroPage.class);
+        basecampMainPage = PageFactory.initElements(driver, PageObjects.Web.MainPage.class);
+        basecampSignInPage = PageFactory.initElements(driver, PageObjects.Web.SignInPage.class);
+        basecampProjectMainPage = PageFactory.initElements(driver, PageObjects.Web.ProjectMainPage.class);
+        basecampUpperMenu = PageFactory.initElements(driver, PageObjects.Web.UpperMenuBar.class);
+        basecampEditProjectDetailsPage = PageFactory.initElements(driver, PageObjects.Web.EditProjectDetails.class);
+        basecampGetHelpIframe = PageFactory.initElements(driver, PageObjects.Web.GetHelpIframe.class);
+        basecampUpperMenuFindWindowPage = PageFactory.initElements(driver, PageObjects.Web.UpperMenuFindWindow.class);
+        basecampSignUpFlow = PageFactory.initElements(driver, PageObjects.Web.SignUpFlow.class);
+        basecampNewProjectFlow = PageFactory.initElements(driver, PageObjects.Web.NewProjectFlow.class);
+        basecampRemoveAProjectFlow = PageFactory.initElements(driver, PageObjects.Web.RemoveAProjectFlow.class);
+        basecampAvatarMenu = PageFactory.initElements(driver, PageObjects.Web.AvatarMenu.class);
+        basecampEditProfilePage = PageFactory.initElements(driver, PageObjects.Web.EditProfilePage.class);
+    }
+
+    public static void initMobile() {
+        mobileBasecampLoginPage = PageFactory.initElements(driver, PageObjects.Mobile.LoginPage.class);
+        mobileBottomMenu = PageFactory.initElements(driver, PageObjects.Mobile.BottomMenu.class);
+        mobileMyStuffPage = PageFactory.initElements(driver, PageObjects.Mobile.MyStuffPage.class);
+    }
 }
