15f150d43a11471480ffbaec86d2d0b6095bcdf9
Ziv Gadri <ziv.automation@gmail.com>
2020-06-07 18:58:01 +0300

    Adding postCredentials method under DbActions class - Posting a new user credentials to DB

--- src/main/java/Extensions/DataBase/DbActions.java
+++ src/main/java/Extensions/DataBase/DbActions.java
@@ -1,4 +1,20 @@
 package Extensions.DataBase;
 
-public class DbActions {
+import Utilities.CommonOps;
+import io.qameta.allure.Step;
+
+public class DbActions extends CommonOps {
+
+    @Step("Post Basecamp new user info to Basecamp Credentials Table")
+    public static void postCredentials() {
+        try {
+            statement = connection.createStatement();
+            String dbop = "INSERT INTO `Basecamp_Users`(`Name`, `Email`, `Password`, `Company`) VALUES ('"
+                    + _userName + "','" + _userEmail + "','" + _password + "','" + _companyName + "');";
+            statement.execute(dbop);
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Error while posting data");
+        }
+    }
 }
