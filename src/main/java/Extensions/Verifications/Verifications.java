75eac466d70abe38650467abd2cb26b2a11b4ee0
Ziv Gadri <ziv.automation@gmail.com>
2020-06-07 18:48:06 +0300

    Adding basic UiActions methods

--- src/main/java/Extensions/Verifications/Verifications.java
+++ src/main/java/Extensions/Verifications/Verifications.java
@@ -1,4 +1,4 @@
-package Extensions.Verivications;           //Assertion methods that are meant to verify our tests. The methods will get called from the tests.
+package Extensions.Verifications;           //Assertion methods that are meant to verify our tests. The methods will get called from the tests.
                                             //The class inherits from CommonOps class
 
 import Utilities.CommonOps;
