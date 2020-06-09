e6d3573690aa0b578eb5feb83feda99bb5beb163
Ziv Gadri <ziv.automation@gmail.com>
2020-06-09 17:16:30 +0300

    Creating signIn and signOut methods under WebFlows class

--- src/main/java/WorkFlows/WebFlows.java
+++ src/main/java/WorkFlows/WebFlows.java
@@ -1,4 +1,36 @@
-package WorkFlows;
+package WorkFlows;              //Represents regular routine tasks and operations that the system does: Creating / removing users,
+                                // new tasks etc. - Inherits from CommonOps
 
-public class WebFlows {
+import Extensions.Web.UiActions;
+import Utilities.CommonOps;
+import io.qameta.allure.Step;
+
+import static Utilities.HelperMethods.getDataFromXML;
+
+public class WebFlows extends CommonOps {
+
+    @Step("Sign in to Basecamp")
+    public static void signIn(String userEmail, String userPassword) {
+        if (basecampUpperMenu.listOfUpperMenuItems.size() == 6) {
+            signOut();
+            UiActions.click(basecampIntroPage.signIn_btn);
+        } else {
+            driver.get(getDataFromXML("URL"));
+            UiActions.click(basecampIntroPage.signIn_btn);
+            if (basecampUpperMenu.listOfUpperMenuItems.size() == 6) {
+                signOut();
+                UiActions.click(basecampIntroPage.signIn_btn);
+            }
+        }
+        UiActions.insertKeysAndClick(basecampSignInPage.userIdentifierInput_Field, userEmail, basecampSignInPage.next_btn);
+        UiActions.insertKeysAndClick(basecampSignInPage.userPasswordInput_Field, userPassword, basecampSignInPage.login_btn);
+    }
+
+    @Step("Sign out from Basecamp")
+    public static void signOut() {
+        UiActions.click(basecampMainPage.homePage_btn);
+        UiActions.click(basecampMainPage.avatarIcon_btn);
+        UiActions.click(basecampAvatarMenu.logOut_btn);
+        driver.get(getDataFromXML("URL"));
+    }
 }
