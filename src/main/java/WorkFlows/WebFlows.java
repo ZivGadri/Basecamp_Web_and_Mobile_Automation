94f4ae25917d6a52c2b751af37ad52a5475af1df
Ziv Gadri <ziv.automation@gmail.com>
2020-06-09 17:18:00 +0300

    Creating signUp method under WebFlows class

--- src/main/java/WorkFlows/WebFlows.java
+++ src/main/java/WorkFlows/WebFlows.java
@@ -1,6 +1,7 @@
 package WorkFlows;              //Represents regular routine tasks and operations that the system does: Creating / removing users,
                                 // new tasks etc. - Inherits from CommonOps
 
+import Extensions.DataBase.DbActions;
 import Extensions.Web.UiActions;
 import Utilities.CommonOps;
 import io.qameta.allure.Step;
@@ -32,5 +33,32 @@ public class WebFlows extends CommonOps {
         UiActions.click(basecampMainPage.avatarIcon_btn);
         UiActions.click(basecampAvatarMenu.logOut_btn);
         driver.get(getDataFromXML("URL"));
+    }
+
+    @Step("Sign up to Basecamp")
+    public static void signUp(String name, String email, String password, String companyName) throws InterruptedException {
+
+        if (basecampUpperMenu.listOfUpperMenuItems.size() == 6) {
+            signOut();
+        } else {
+            driver.get(getDataFromXML("URL"));
+            UiActions.click(basecampIntroPage.tryItFree_btn);
+            UiActions.insertKeys(basecampSignUpFlow.name_Field, name);
+            Thread.sleep(1500);
+            UiActions.insertKeysAndClick(basecampSignUpFlow.email_Field, email, basecampSignUpFlow.nextButtonFirst_btn);
+            Thread.sleep(1500);
+            UiActions.insertKeysAndClick(basecampSignUpFlow.password_Field, password, basecampSignUpFlow.createMyAccount_btn);
+            Thread.sleep(1500);
+            UiActions.insertKeysAndClick(basecampSignUpFlow.companyName_Field, companyName, basecampSignUpFlow.nextButtonSecond_btn);
+            Thread.sleep(1500);
+            UiActions.click(basecampSignUpFlow.nextButtonThird_btn);
+            Thread.sleep(1500);
+            UiActions.click(basecampSignUpFlow.nextButtonForth_btn);
+            Thread.sleep(1500);
+            UiActions.click(basecampSignUpFlow.allDone_btn);
+            DbActions.postCredentials();
+            Thread.sleep(1500);
+            basecampSignUpFlow.noThanks_btn.click();
+        }
     }
 }
