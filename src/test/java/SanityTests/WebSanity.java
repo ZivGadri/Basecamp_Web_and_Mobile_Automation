407b8a1ff80418a8985038b5ca1743d9bae05982
Ziv Gadri <ziv.automation@gmail.com>
2020-06-09 17:33:30 +0300

    Creating testLogin under WebSanity class

--- src/test/java/SanityTests/WebSanity.java
+++ src/test/java/SanityTests/WebSanity.java
@@ -1,4 +1,21 @@
 package SanityTests;
 
-public class WebSanity {
+import Extensions.Verifications.Verifications;
+import Utilities.CommonOps;
+import WorkFlows.WebFlows;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static Utilities.HelperMethods.getDataFromXML;
+
+@Listeners(Utilities.Listeners.class)
+public class WebSanity extends CommonOps {
+
+    @Test(description = "Login Sanity Test", priority = 2, enabled = true)
+    @Description("Test description: Logging in with a user")
+    public void testLogin() {
+        WebFlows.signIn(getDataFromXML("UserEmail"), getDataFromXML("Password"));
+        Verifications.verifyTextInElement(basecampMainPage.pageTitle_txt, "Your Projects");
+    }
 }
