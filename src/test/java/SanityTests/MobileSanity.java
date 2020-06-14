2ec8b998e1badf87bc189ad30a8e4c8c10f40920
Ziv Gadri <ziv.automation@gmail.com>
2020-06-14 21:49:46 +0300

    Adding a sanity test of mobile app login

--- src/test/java/SanityTests/MobileSanity.java
+++ src/test/java/SanityTests/MobileSanity.java
@@ -1,6 +1,21 @@
 package SanityTests;
 
+import Extensions.Verifications.Verifications;
+import Utilities.CommonOps;
+import WorkFlows.MobileFlows;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
-public class MobileSanity {
+import static Utilities.HelperMethods.getDataFromXML;
 
+@Listeners(Utilities.Listeners.class)
+public class MobileSanity extends CommonOps {
+
+    @Test(description = "Mobile Login Sanity Test")
+    @Description("Test description: Logging in with a user to mobile app")
+    public void testLoginMobile() throws InterruptedException {
+        MobileFlows.login(getDataFromXML("UserEmail"), getDataFromXML("Password"));
+        Verifications.verifyTextInElement(basecampMainPage.pageTitle_txt, "Your Projects");
+    }
 }
